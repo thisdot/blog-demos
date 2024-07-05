f519f81334c50c47a5ecf578e973fae065c4ea34
Dario Djuric <dario.djuric@gmail.com>
2024-07-05 12:28:21 +0200

    Clean up NR script

--- next-new-relic-integration/app/components/new-relic-script.tsx
+++ next-new-relic-integration/app/components/new-relic-script.tsx
@@ -2,20 +2,24 @@ import Script from 'next/script';
 import { IS_PRODUCTION, IS_STAGING } from '@/utilities/environment';
 import { DEV_SCRIPT, PRODUCTION_SCRIPT, STAGING_SCRIPT } from '@/app/components/new-relic-scripts-per-env';
 
+function getScriptContent() {
+  if (IS_PRODUCTION) {
+    return PRODUCTION_SCRIPT;
+  }
+
+  if (IS_STAGING) {
+    return STAGING_SCRIPT;
+  }
+
+  return DEV_SCRIPT;
+}
+
 export default function NewRelicScript() {
-  const scriptContent = IS_PRODUCTION
-    ? PRODUCTION_SCRIPT
-    : IS_STAGING
-      ? STAGING_SCRIPT
-      : DEV_SCRIPT;
+  const scriptContent = getScriptContent();
 
   return (
     <Script
-      // We have to set an id for inline scripts.
-      // See https://nextjs.org/docs/app/building-your-application/optimizing/scripts#inline-scripts
       id="nr-browser-agent"
-      // By setting the strategy to "beforeInteractive" we guarantee that
-      // the script will be added to the document's `head` element.
       strategy="beforeInteractive"
     >
       {scriptContent}
